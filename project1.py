987b597428ce765b279921b9a512dee95edca62f
Prosper Archer <prosperarcher4@gmail.com>
2025-06-16 14:12:59 +0100

    Create project1.py

---
+++ project1.py
@@ -0,0 +1,11 @@
+# Project 1: Hello World and Basic Arithmetic
+
+# Print a greeting message
+print("Hello, World!")
+
+# Basic arithmetic operations
+print("2 + 2 =", 2 + 2)
+print("5 * 3 =", 5 * 3)
+print("10 - 4 =", 10 - 4)
+print("8 / 2 =", 8 / 2)
+print("2 ** 3 =", 2 ** 3)  # Exponentiation
