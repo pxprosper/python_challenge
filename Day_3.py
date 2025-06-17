e3f1f6f90157b3efcf3a3d8ed0ab894ba63545e1
Prosper Archer <prosperarcher4@gmail.com>
2025-06-17 22:23:21 +0100

    Add files via upload

---
+++ Day_3.py
@@ -0,0 +1,8 @@
+# Get the user's age
+age = int(input("Enter your age: "))
+
+# Check eligibility
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are too young to vote.")
